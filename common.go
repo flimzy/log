30d0502275768dd2616f21768f714364019f3bb5
Jonathan Hall <flimzy@flimzy.com>
2016-10-06 22:54:30 +0200

    Add support for Logger type

--- common.go
+++ common.go
@@ -1,8 +1,22 @@
 package log
 
 import (
+	"io"
 	golog "log"
 )
+
+// A Logger represents an active logging object that generates lines of output
+// to an io.Writer. Each logging operation makes a single call to the Writer's
+// Write method. A Logger can be used simultaneously from multiple goroutines;
+// it guarantees to serialize access to the Writer.
+type Logger struct {
+	golog *golog.Logger
+}
+
+// New creates a new Logger.
+func New(out io.Writer, prefix string, flag int) *Logger {
+	return &Logger{golog.New(out, prefix, flag)}
+}
 
 // Print calls log.Print()
 func Print(v ...interface{}) {
@@ -17,4 +31,19 @@ func Printf(format string, v ...interface{}) {
 // Println calls log.Println()
 func Println(v ...interface{}) {
 	golog.Println(v...)
+}
+
+// Print calls log.Print()
+func (l *Logger) Print(v ...interface{}) {
+	l.golog.Print(v...)
+}
+
+// Printf calls log.Printf()
+func (l *Logger) Printf(format string, v ...interface{}) {
+	l.golog.Printf(format, v...)
+}
+
+// Println calls log.Println()
+func (l *Logger) Println(v ...interface{}) {
+	l.golog.Println(v...)
 }
--- debug.go
+++ debug.go
@@ -20,3 +20,18 @@ func Debugf(format string, v ...interface{}) {
 func Debugln(v ...interface{}) {
 	golog.Println(v...)
 }
+
+// Debug calls log.Print() only when the 'debug' build flag is present.
+func (l *Logger) Debug(v ...interface{}) {
+	l.Print(v...)
+}
+
+// Debugf calls log.Printf() only when the 'debug' build flag is present.
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.Printf(format, v...)
+}
+
+// Debugln calls log.Println() only when the 'debug' build flag is present.
+func (l *Logger) Debugln(v ...interface{}) {
+	l.Println(v...)
+}
